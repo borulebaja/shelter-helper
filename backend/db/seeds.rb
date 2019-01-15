a6c26cc28d8564aa6adeb165be426d0460fdea47
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2019-01-14 19:05:35 -0600

    updated controllers/login works

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -11,8 +11,9 @@ ShelterLocation.destroy_all
 Need.destroy_all
 
 user1 = User.create(name: "alan", email: "alan@example.com", password: "0000")
+user2 = User.create(name: "bims", email: "bims@example.com", password: "0000")
 
-shelter_location1 = ShelterLocation.create(name: 'test', address: '708 Main St, Houston TX, 77002', user_id: 1)
-shelter_location2 = ShelterLocation.create(name: "test2", address: "1625 Main St, Houston, TX, 77002", user_id: 1)
+shelter_location1 = ShelterLocation.create(name: 'first_shelter', address: '708 Main St, Houston TX, 77002', user_id: user1.id)
+shelter_location2 = ShelterLocation.create(name: "second_shelter", address: "1625 Main St, Houston, TX, 77002", user_id: user2.id)
 
-Need.create(name_of_needs: "blankets", shelter_location_id: 1)
+Need.create(name_of_needs: "blankets", shelter_location_id: shelter_location1.id)
